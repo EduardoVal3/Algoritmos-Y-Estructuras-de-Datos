c1339134bdf5de25758114510a6e2591847cff48
levittwl <156035992+EduardoVal3@users.noreply.github.com>
2024-12-16 19:56:50 -0600

    Add files via upload

---
+++ Guia De Ejercicios/17. Invertir una Lista Enlazada/invertirListaInlazada.py
@@ -0,0 +1,13 @@
+# 17
+class ListaEnlazada:
+    # Métodos anteriores...
+
+    def invertir(self):
+        previo = None
+        actual = self.cabeza
+        while actual:
+            siguiente = actual.siguiente
+            actual.siguiente = previo
+            previo = actual
+            actual = siguiente
+        self.cabeza = previo
